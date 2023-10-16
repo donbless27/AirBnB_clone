19f1a24daa1464aeb076be6ff3b666e432570a73
donbless27 <donbless0527@gmail.com>
2023-10-16 05:56:44 -0700

    The AirBnB Clone Console Team Project 0x00

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines the base model"""
 
-import models
+import uuid
 from datetime import datetime
-from uuid import uuid4
+from models import storage
 
 
 class BaseModel:
@@ -30,7 +30,7 @@ class BaseModel:
                     self.id = str(uuid.uuid4())
                     self.created_at = datetime.now()
                     self.updated_at = datetime.now()
-                    models.storage.new(self)
+                    storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
@@ -42,7 +42,7 @@ class BaseModel:
         """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """This returns a dictionary containing all keys/values of __dict__"""
